a4e9d1d0a85bf9ddf33cd67ceb086a1459c0ca94
jaseruddin <juddin@ocir.on.ca>
2019-07-30 14:54:57 -0400

    Added a filter to substitute query string with blank

--- python/format_errors.py
+++ python/format_errors.py
@@ -1,8 +1,15 @@
+import re
+
 def format_tuple(name, args):
     if len(args) == 1:
+        args[0] = filter_sensitive(args[0])
         return f"{name}({args[0]})"
+
+    args = tuple(filter_sensitive(arg) for arg in args)
     return f"{name}{args}"
 
+def filter_sensitive(arg):
+    return re.sub(r"(\?[^\s]+)"," ",str(arg))
 
 def format_exception(e):
     return format_tuple(e.__class__.__name__, e.args)
