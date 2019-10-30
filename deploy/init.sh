c9381628923f96fd34ae2b6397679fda715af1c2
khartmann <kevin.hartmann@oicr.on.ca>
2019-10-29 20:31:27 -0400

    Change: Renamed "port" to SERVER_PORT

--- deploy/init.sh
+++ deploy/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . /daco2ego/set_vars.sh
-thttpd -d /uploads -c /upload.cgi -p ${PORT:-8081}
+thttpd -d /uploads -c /upload.cgi -p ${SERVER_PORT:-8081}
 /daco2ego/monitor.sh
--- deploy/set_vars.sh
+++ deploy/set_vars.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #set -x
-: ${PORT:=8080}
+: ${SERVER_PORT:=8081}
 : ${CLIENT_SECRET:="shhh!"}
 : ${CLIENT_ID:="daco2ego"}
 : ${AES_KEY:="a1a2a3a4a5a6a7a8a9aa"}
 : ${AES_IV:="b1b2b3b4b5b6b7b8b9bb"}
-: ${SLACK_URL:="http://localhost:${PORT}/report.cgi"}
+: ${SLACK_URL:="http://localhost:${SERVER_PORT}/report.cgi"}
 : ${EGO_URL:="https://ego.dev.argo.cancercollaboratory.org/api"}
 : ${DACO_GROUP:="DACO"}
 : ${CLOUD_GROUP:="CLOUD"}
@@ -18,7 +18,7 @@ if [ "${VAULT_ENABLED}" = "true" ];then
     jwt=$(cat ${VAULT_TOKEN_FILE})
 
     # use the jwt auth token to get a vault authorization token for our role 
-    vault_token=$(curl --request POST -data "{\"jwt\": \"${jwt}\", \"role\": \"${VAULT_ROLE}\"}" ${VAULT_URL}/${VAULT_LOGIN_PATH} | jq -r .auth.client_token)
+    vault_token=$(curl ${VAULT_URL}/${VAULT_LOGIN_PATH} -data "{\"jwt\": \"${jwt}\", \"role\": \"${VAULT_ROLE}\"}" | jq -r .auth.client_token)
 
     # use our authorization token to get our secrets
     curl --header "X-Vault-Token: $vault_token" ${VAULT_URL}/{VAULT_SECRETS_PATH} > secrets
@@ -30,4 +30,4 @@ if [ "${VAULT_ENABLED}" = "true" ];then
     AES_KEY=$(cat secrets | jq -r .data.data.AES_KEY) 
     SLACK_URL=$(cat secrets | jq -r .data.data.SLACK_URL)
 fi
-export PORT CLIENT_SECRET CLIENT_ID AES_KEY AES_IV SLACK_URL EGO_URL DACO_GROUP CLOUD_GROUP DACO_FILE CLOUD_FILE LOG_FILE
+export SERVER_PORT CLIENT_SECRET CLIENT_ID AES_KEY AES_IV SLACK_URL EGO_URL DACO_GROUP CLOUD_GROUP DACO_FILE CLOUD_FILE LOG_FILE
