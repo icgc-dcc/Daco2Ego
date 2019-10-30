73be07b62ec9f34c9d109bf717a133ec63b7060e
khartmann <kevin.hartmann@oicr.on.ca>
2019-10-30 13:41:56 -0400

    Bugfix: Fixed typos in curl commands, bad URL paths, and bad json paths in deploy/set_vars.sh

--- deploy/set_vars.sh
+++ deploy/set_vars.sh
@@ -18,16 +18,16 @@ if [ "${VAULT_ENABLED}" = "true" ];then
     jwt=$(cat ${VAULT_TOKEN_FILE})
 
     # use the jwt auth token to get a vault authorization token for our role 
-    vault_token=$(curl ${VAULT_URL}/${VAULT_LOGIN_PATH} -data "{\"jwt\": \"${jwt}\", \"role\": \"${VAULT_ROLE}\"}" | jq -r .auth.client_token)
+    vault_token=$(curl ${VAULT_URL}/v1${VAULT_LOGIN_PATH} --data "{\"jwt\": \"${jwt}\", \"role\": \"${VAULT_ROLE}\"}" | jq -r .auth.client_token)
 
     # use our authorization token to get our secrets
-    curl --header "X-Vault-Token: $vault_token" ${VAULT_URL}/{VAULT_SECRETS_PATH} > secrets
+    curl --header "X-Vault-Token: $vault_token" --LIST ${VAULT_URL}/v1/${VAULT_SECRETS_PATH%/} > secrets
 
     # pull our secrets out the JSON file and into environment variables 
-    CLIENT_ID=$(cat secrets | jq -r .data.data.CLIENT_ID) 
-    CLIENT_SECRET=$(cat secrets | jq -r .data.data.CLIENT_SECRET) 
-    AES_IV=$(cat secrets | jq -r .data.data.AES_IV) 
-    AES_KEY=$(cat secrets | jq -r .data.data.AES_KEY) 
-    SLACK_URL=$(cat secrets | jq -r .data.data.SLACK_URL)
+    CLIENT_ID=$(cat secrets | jq -r .data.CLIENT_ID) 
+    CLIENT_SECRET=$(cat secrets | jq -r .data.CLIENT_SECRET) 
+    AES_IV=$(cat secrets | jq -r .data.AES_IV) 
+    AES_KEY=$(cat secrets | jq -r .data.AES_KEY) 
+    SLACK_URL=$(cat secrets | jq -r .data.SLACK_URL)
 fi
 export SERVER_PORT CLIENT_SECRET CLIENT_ID AES_KEY AES_IV SLACK_URL EGO_URL DACO_GROUP CLOUD_GROUP DACO_FILE CLOUD_FILE LOG_FILE
